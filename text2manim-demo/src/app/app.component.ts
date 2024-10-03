f8bdd4e4d118f6b2587fc891e3bc8939dc3d9840
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-10-03 20:51:24 +0900

    feat: Add default theme

--- text2manim-demo/src/app/app.component.ts
+++ text2manim-demo/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
