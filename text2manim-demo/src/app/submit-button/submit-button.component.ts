9c66c4cc8088bdf7b49a908d8ff56e09bc31a83a
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-10-08 20:55:09 +0900

    feat: Add progress spinner

--- text2manim-demo/src/app/submit-button/submit-button.component.ts
+++ text2manim-demo/src/app/submit-button/submit-button.component.ts
@@ -1,21 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-submit-button',
   standalone: true,
   imports: [
     MatIconModule,
+    MatProgressSpinnerModule,
+    CommonModule,
   ],
   templateUrl: './submit-button.component.html',
   styleUrl: './submit-button.component.css'
 })
 export class SubmitButtonComponent {
   @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
   @Output() submit = new EventEmitter<void>();
 
   onClick(): void {
-    this.submit.emit();
+    if (!this.disabled && !this.loading) {
+      this.submit.emit();
+    }
   }
 }
 
